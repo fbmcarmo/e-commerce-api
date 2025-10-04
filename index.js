14580e1584fed7c8efb04102e255e63a69a8f7ce
fbmcarmo <fbrunomcarmo@gmail.com>
2025-10-03 22:12:08 -0300

    Novas alteracoes

--- index.js
+++ index.js
@@ -2,17 +2,43 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 //http://localhost:3000
 //http://192.168.1.100:3000
 
 const PORT = 4467;
+const products = [];
 
 //API REST FULL
 // Rota - Endpoint - callback
 // ====> Req -> Request - Requisição
 // <==== Res -> Response - Resposta
 app.get("/", (req, res) => {
-    res.send("Olá Mundo!");
+    res.send("Olá Bruno, seja bem vindo");
+})
+
+// C - Create - POST
+// R - Read - GET
+// U - Update - PUT
+// D - Delete - DELETE
+
+app.post("/products", (req, res) => {
+    const { name, category, price } = req.body;
+    
+    products.push({
+        name,
+        category,
+        price
+    })
+
+    res.status(201).send({
+        message: "Produto criado com sucesso"
+    })
+})
+
+app.get("/products", (req, res) => {
+    res.send(products) 
 })
 
 app.listen(PORT, () => {
