32ab72022d3952793984ee493d391894f4351998
fbmcarmo <fbrunomcarmo@gmail.com>
2025-12-03 22:03:54 -0300

    microsservice

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const productsController = require("../controllers/products")
 const productsMiddleware = require("../middlewares/products")
 const { authToken } = require("../middlewares/authToken")
+const axios = require("axios")
 
 router.post(
     "/products", 
@@ -15,5 +16,20 @@ router.get(
     "/products",
     productsController.getAllProducts
 )
+
+router.get("/products/service", async (req, res) => {
+    try {
+        const response = await axios.get("http://localhost:4505", {
+            headers: {
+                "Authorization": process.env.SECRET_KEY_EMAIL_API
+            }
+        })
+        return res.send(response.data)
+    } catch (error) {
+        return res.status(500).send({
+            error: error.message
+        })
+    }
+})
 
 module.exports = router
